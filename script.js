0627c8b3104f451af2a0ba3f8d242b764d2f257b
alexannaherbelin <alexannaherbelin@students.berkeley.net>
2025-08-14 18:41:57 +0000

    weeeeeee

--- script.js
+++ script.js
@@ -3,10 +3,55 @@
  * Run after you make changes to re-run the tests with your new code.
  */
 
-function sleep_in(weekday,vacation){
-    return true;
+function countEvens(arr){
+    let count = 0;
+    for(let i = 0; i<arr.length; i++){
+        if(arr[i]%2==0){
+         count++;
+        }
+    }
+    return count;
 }
 
-function string_times(str,num){
-    return str;
+function countHi(str){
+    let count = 0;
+    for(let i = 0; i<str.length; i++){
+        if(str[i]== "h" && str[i+1]=="i"){
+            count++;
+        }
+    }
+    return count;
+}
+
+function no14(arr){
+    return !(arr.includes(1) && arr.includes(4))
+}
+
+function either24(arr){
+    let ans1 = false;
+    let ans2 = false;
+    for(let i = 0; i<arr.length; i++){
+        if(arr[i]==2 && arr[i+1]==2){
+            ans1=true;
+        }
+        if (arr[i]==4 && arr[i+1]==4){
+            ans2=true;
+        }
+    }
+    if (ans2==ans1){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function makeChocolate(small, big, goal){
+    let smallNeeded = goal%5
+    let a;
+    let b;
+    if (small>5){
+        a = small%5
+        b = (small-a)/5
+    }
+
 }
